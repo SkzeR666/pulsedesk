76f46073a554901d7387d4d9eeb509d9856aaf97
Augusto Santos <augustowebdev0@gmail.com>
2026-03-24 16:00:58 -0300

    Remove invalid HeroUI provider import

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,22 +1,16 @@
 'use client'
 
-import { RouterProvider } from '@heroui/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { AppProvider } from '@/lib/app-context'
-import { useRouter } from 'next/navigation'
-import { Toaster } from '@/components/ui/sonner'
+import { Toaster } from 'sonner'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    const router = useRouter()
-
     return (
-        <RouterProvider navigate={router.push}>
-            <NextThemesProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-                <AppProvider>
-                    {children}
-                    <Toaster richColors position="top-right" />
-                </AppProvider>
-            </NextThemesProvider>
-        </RouterProvider>
+        <NextThemesProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+            <AppProvider>
+                {children}
+                <Toaster richColors position="top-right" />
+            </AppProvider>
+        </NextThemesProvider>
     )
 }
